2178336bf9bdd98bc1eede0dba7e9fe578dba3ad
kuldeep7688 <7688kuldeepsingh@gmail.com>
2026-08-28 20:37:35 -0400

    fix: add dummy exports to content.ts for build compatibility

--- src/config/content.ts
+++ src/config/content.ts
@@ -1,3 +1,14 @@
 export const UNLOCK_DATE = new Date('2026-09-03T00:00:01')
 
 export const herName = 'Archita'
+
+export const songSrc = ''
+export const heroSubtitle = ''
+export const reasonsILoveYou: { title: string; text: string }[] = []
+export const photos: { src: string; caption: string; message: string }[] = []
+export const scratchMessages: string[] = []
+export const cakePrompt = ''
+export const cakeFallback = ''
+export const finalMessage = ''
+export const candleCount = 0
+export const timeline: { icon: string; title: string; date: string; description: string; photo: string }[] = []
